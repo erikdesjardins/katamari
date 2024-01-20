5abaa10e9f9631b2e28bf5da616a471909d128db
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2024-01-20 16:32:14 -0500

    move route definition to submod

--- src/server.rs
+++ src/server.rs
@@ -6,12 +6,14 @@ use tower::ServiceBuilder;
 use tower_http::compression::CompressionLayer;
 use tower_http::trace::TraceLayer;
 
+mod index;
+
 pub async fn run(addr: SocketAddr) -> Result<(), io::Error> {
     let listener = tokio::net::TcpListener::bind(addr).await?;
 
     tracing::info!("listening on {}", addr);
 
-    let app = Router::new().route("/", get(index)).layer(
+    let app = Router::new().route("/", get(index::get)).layer(
         ServiceBuilder::new()
             .layer(TraceLayer::new_for_http())
             .layer(CompressionLayer::new().br(true)),
@@ -20,8 +22,4 @@ pub async fn run(addr: SocketAddr) -> Result<(), io::Error> {
     axum::serve(listener, app).await?;
 
     Ok(())
-}
-
-async fn index() -> &'static str {
-    "Hello, World!"
 }
