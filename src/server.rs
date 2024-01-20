1070bd52a4df29642b64f5de4b02f5124b45d8e8
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2024-01-20 17:06:05 -0500

    add http client, enable http2

--- src/server.rs
+++ src/server.rs
@@ -1,23 +1,48 @@
 use axum::routing::get;
 use axum::Router;
+use http_body_util::Empty;
+use hyper::body::Bytes;
+use hyper_rustls::{HttpsConnector, HttpsConnectorBuilder};
+use hyper_util::client::legacy::connect::HttpConnector;
+use hyper_util::client::legacy::Client;
+use hyper_util::rt::TokioExecutor;
 use std::io;
 use std::net::SocketAddr;
+use std::sync::Arc;
 use tower::ServiceBuilder;
 use tower_http::compression::CompressionLayer;
 use tower_http::trace::TraceLayer;
 
 mod index;
 
+struct AppState {
+    client: Client<HttpsConnector<HttpConnector>, Empty<Bytes>>,
+}
+
 pub async fn run(addr: SocketAddr) -> Result<(), io::Error> {
     let listener = tokio::net::TcpListener::bind(addr).await?;
 
     tracing::info!("listening on {}", addr);
 
-    let app = Router::new().route("/", get(index::get)).layer(
-        ServiceBuilder::new()
-            .layer(TraceLayer::new_for_http())
-            .layer(CompressionLayer::new().br(true)),
+    let client = Client::builder(TokioExecutor::new()).build(
+        HttpsConnectorBuilder::new()
+            .with_native_roots()?
+            .https_or_http()
+            .enable_http1()
+            .enable_http2()
+            .build(),
     );
+
+    let state = Arc::new(AppState { client });
+
+    let app = Router::new()
+        .route("/", get(index::get))
+        .with_state(state)
+        .layer(
+            ServiceBuilder::new()
+                .layer(TraceLayer::new_for_http())
+                .layer(CompressionLayer::new().br(true)),
+        );
 
     axum::serve(listener, app).await?;
 
--- src/server/index.rs
+++ src/server/index.rs
@@ -1,3 +1,15 @@
-pub async fn get() -> &'static str {
-    "Hello, World!"
+use crate::server::AppState;
+use axum::extract::State;
+use axum::response::{Html, IntoResponse};
+use http_body_util::BodyExt;
+use std::sync::Arc;
+
+pub async fn get(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    let output = state
+        .client
+        .get("https://erikdesjardins.io/".parse().unwrap())
+        .await
+        .unwrap();
+
+    Html(output.into_body().collect().await.unwrap().to_bytes())
 }
